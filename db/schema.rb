115471b314003f1a7e972136e5a2a0dba2a17686
KnighT178 <egor.skandakov@gmail.com>
2021-12-10 03:38:25 +0300

    fixed Answer, Test & Question models

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_003326) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
