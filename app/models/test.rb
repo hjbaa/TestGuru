92a6db00c2ffd769a74da6bf1f53d009431458d5
KnighT178 <egor.skandakov@gmail.com>
2021-12-12 16:52:21 +0300

    Fixed solution for part#4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.tests_by_category(category_name)
-    Test.joins('JOIN categories').select(:title).order(title: :desc).where('tests.category_id = categories.id').and(
+    Test.joins('JOIN categories').order(title: :desc).where('tests.category_id = categories.id').and(
       Category.where(title: category_name)
     ).pluck(:title)
   end
