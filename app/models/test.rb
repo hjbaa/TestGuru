2cf7a9b2dc09951d447e5cac08d681ca182f12fa
KnighT178 <egor.skandakov@gmail.com>
2021-12-14 10:54:13 +0300

    Fixed solution

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,7 @@
 class Test < ApplicationRecord
-  belongs_to :category
-  belongs_to :user, foreign_key: 'author_id'
-  has_many :questions
-  has_many :tests_by_users, dependent: :destroy
-  has_many :users, through: :tests_by_users
-
   def self.tests_by_category(category_name)
-    Test.joins(:category).where(categories: { title: category_name }).order(title: :desc).pluck(:title)
+    Test.joins('INNER JOIN categories on tests.category_id = categories.id').where(
+      categories: { title: category_name }
+    ).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_by_users
 
   def passed_tests(tests_level)
-    Test.joins(:users).where(tests: { level: tests_level }, tests_by_users: { user_id: id })
+    Test.joins('INNER JOIN tests_by_users ON tests_by_users.test_id = tests.id').where(
+      tests: { level: tests_level }, tests_by_users: { user_id: id }
+    )
   end
 end
