6f2ecc8ad9bd23ef21f32047c9211d73173f3d81
KnighT178 <egor.skandakov@gmail.com>
2021-12-14 19:18:57 +0300

    Fixed solution

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,8 @@
 class Test < ApplicationRecord
   def self.tests_by_category(category_name)
-    Test.joins('INNER JOIN categories on tests.category_id = categories.id').where(
-      categories: { title: category_name }
-    ).order(title: :desc).pluck(:title)
+    Test.joins('INNER JOIN categories on tests.category_id = categories.id')
+        .where(categories: { title: category_name })
+        .order(title: :desc)
+        .pluck(:title)
   end
 end
