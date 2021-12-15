bda4ea1ab0c54c5acccc8f143ff1a0128b99f951
KnighT178 <egor.skandakov@gmail.com>
2021-12-16 02:26:19 +0300

    Reformatted few files

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,8 @@
 class Test < ApplicationRecord
-  has_many :questions
   belongs_to :category
   belongs_to :author, class_name: 'User'
+
+  has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
 
