03d84c5ce368b2b0c86e77714d43260872f7e59a
KnighT178 <egor.skandakov@gmail.com>
2021-12-18 17:35:18 +0300

    Reformatted code for part #6

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,15 +3,8 @@
 # Модель вопроса
 class Question < ApplicationRecord
   validates :title, presence: true
-  # validate :validates_max_answers_num
 
   belongs_to :test
 
   has_many :answers, dependent: :destroy
-
-  #private
-
-  # def validates_max_answers_num
-  #   errors.add(:answers) if answers.count > 4
-  # end
 end
