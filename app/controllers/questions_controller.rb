e0df2bce57cf05e7748be8e24378b21082fab793
KnighT178 <egor.skandakov@gmail.com>
2021-12-29 16:41:58 +0300

    Corrected mistakes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  # rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
   before_action :find_question, only: %i[show edit update]
   before_action :find_test, only: %i[new create]
 
