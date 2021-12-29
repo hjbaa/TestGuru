e7010b9b2b27daf9cb017f0cc4189cf7eba81385
KnighT178 <egor.skandakov@gmail.com>
2021-12-29 19:33:45 +0300

    Fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-  before_action :find_question, only: %i[show edit update]
+  before_action :find_question, only: %i[show edit update destroy]
   before_action :find_test, only: %i[new create]
 
   def index
