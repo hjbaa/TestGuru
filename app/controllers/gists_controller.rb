40bc74ec867d0be8fc2122fc795c1430e114a63a
KnighT178 <egor.skandakov@gmail.com>
2022-01-13 16:34:54 +0300

    Refactored

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GistsController < ApplicationController
   def create
     find_test_passage
--- lib/clients/github_client.rb
+++ lib/clients/github_client.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class GithubClient
   ROOT_ENDPOINT = 'https://api.github.com'
-
 
   def initialize
     @http_client = setup_http_client
